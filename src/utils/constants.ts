a084b4f620f0742ba86e36b73c439b17e0b32f37
lakshay kamra <lakshay.kamra@iongroup.com>
2025-11-22 02:54:49 +0530

    githubpages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const WHATSAPP_NUMBER = "+911234567890" // replace with real number, include country code
+export const WHATSAPP_NUMBER = "+91858601776" // replace with real number, include country code
 export const CONTACT_EMAIL = "contact@jiwan2live.example" // replace with real email
